ce1002e25f7c3987e3db24b3641b8e5810c1d16c
Avidan Baruchi <avidanb@code-college.net>
2016-07-01 13:06:44 +0300

    simple solution, need to fix it

--- Lab10_exercise_2/FileFinder/Program.cs
+++ Lab10_exercise_2/FileFinder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,49 @@ namespace FileFinder
     {
         static void Main(string[] args)
         {
+            ICollection<string> foundFiles = null;
+
+            if(args.Length != 2)
+            {
+                Console.WriteLine("Please supply directory path and a search string and try again!");
+                return;
+            }
+
+            if(Directory.Exists(args[0]))
+            {
+                foundFiles = SearchDirectoryAndDisplayFiles(args[0], args[1]);
+            }
+            else
+            {
+                Console.WriteLine("directory do not exists!");
+            }
+        }
+
+        private static ICollection<string> SearchDirectoryAndDisplayFiles(string directory, string name)
+        {
+            List<string> foundPaths = new List<string>();
+            ICollection<string> subDirectoryFoundPaths = new List<string>();
+
+            if(Directory.Exists(directory))
+            {
+                foreach (string subDirectory in Directory.GetDirectories(directory))
+                {
+                    foreach (string file in Directory.GetFiles(subDirectory, name))
+                    {
+                        foundPaths.Add(file);
+                    }
+
+                    subDirectoryFoundPaths = SearchDirectoryAndDisplayFiles(subDirectory, name);
+                    foundPaths.AddRange(subDirectoryFoundPaths);
+                }
+            }
+
+            foreach (string file in Directory.GetFiles(directory, name))
+            {
+                foundPaths.Add(file);
+            }
+
+            return foundPaths;
         }
     }
 }
