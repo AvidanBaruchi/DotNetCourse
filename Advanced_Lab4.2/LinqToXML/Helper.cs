14fda4cf06efac32a03e1faef8f843de15640a62
Avidan Baruchi <avidanb@code-college.net>
2016-07-18 23:53:45 +0300

    finished xml build, solved a and b from a-e

---
+++ Advanced_Lab4.2/LinqToXML/Helper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinqToXML
+{
+    class Helper
+    {
+        public void ListOfProperties(XElement xml)
+        {
+            var query = from type in xml.Descendants("Type")
+                        let typeName = (string)type.Attribute("FullName")
+                        orderby typeName
+                        where !type.Element("Properties").HasElements
+                        select typeName;
+
+            Console.WriteLine($"There are {query.Count()} Types without properties");
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void MethodsCount(XElement xml)
+        {
+            var query = (from method in xml.Descendants("Method")
+                         select method).Count();
+
+            Console.WriteLine($"There are {query} methods!");
+        }
+    }
+}
--- Advanced_Lab4.2/LinqToXML/Program.cs
+++ Advanced_Lab4.2/LinqToXML/Program.cs
@@ -12,8 +12,10 @@ namespace LinqToXML
     {
         static void Main(string[] args)
         {
+            Helper helper = new Helper();
+
             var xml = from type in Assembly.GetAssembly(typeof(string)).GetTypes()
-                      where type.IsPublic && type.IsClass && type.Name == "Exception"
+                      where type.IsPublic && type.IsClass //&& type.Name == "Timer"//&& type.Name == "Exception"
                       select new XElement("Type",
                       new XAttribute("FullName", type.FullName),
                       new XElement("Properties",
@@ -23,12 +25,20 @@ namespace LinqToXML
                       new XAttribute("Type", prop.PropertyType))),
                       new XElement("Methods", 
                       from method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                      where !method.IsSpecialName
                       select new XElement("Method", 
                       new XAttribute("Name", method.Name),
-                      new XAttribute("ReturnType", method.ReturnType.Name))));
+                      new XAttribute("ReturnType", method.ReturnType.Name),
+                      new XElement("Parameters", 
+                      from param in method.GetParameters()
+                      select new XElement("Parameter", 
+                      new XAttribute("Name", param.Name),
+                      new XAttribute("Type", param.ParameterType.FullName ?? param.ParameterType.Name))))));
 
             XElement asXml = new XElement("Types", xml);
-            Console.WriteLine(asXml);
+            //Console.WriteLine(asXml);
+            //helper.ListOfProperties(asXml);
+            helper.MethodsCount(asXml);
             Console.ReadLine();
         }
     }
