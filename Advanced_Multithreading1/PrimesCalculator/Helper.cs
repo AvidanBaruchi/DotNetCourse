52a961b16ffea2805333a0db551ccab01a1017e4
Avidan Baruchi <avidanb@code-college.net>
2016-07-22 13:00:30 +0300

    added Multithreading 1

---
+++ Advanced_Multithreading1/PrimesCalculator/Helper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimesCalculator
+{
+    class Helper
+    {
+        public int[] CalcPrimes(int from, int to)
+        {
+            int[] result = null;
+            ArrayList collection = new ArrayList();
+
+            if (from < 2)
+            {
+                from = 2;
+            }
+
+            if (from % 2 == 0)
+            {
+                if (from == 2 && to >= from)
+                {
+                    collection.Add(2);
+                }
+
+                from++;
+            }
+
+            for (int currentNumber = from; currentNumber <= to; currentNumber += 2)
+            {
+                if (IsPrime(currentNumber))
+                {
+                    collection.Add(currentNumber);
+                }
+            }
+
+            result = new int[collection.Count];
+            collection.CopyTo(result);
+
+            return result;
+        }
+
+        private bool IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            if (number == 2)
+            {
+                return true;
+            }
+
+            if (number % 2 == 0)
+            {
+                return false;
+            }
+
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if ((number % i) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Advanced_Multithreading1/PrimesCalculator/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrimesCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Regex number = new Regex("^[0-9]+$");
+        private SynchronizationContext _synchronizationContext = SynchronizationContext.Current;
+        Helper _helper = new Helper();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Clear();
+            CalculatePrimes();
+        }
+
+        private void CalculatePrimes()
+        {
+            int from = int.Parse(textBoxFrom.Text);
+            int to = int.Parse(textBoxTo.Text);
+
+            Task.Run(() =>
+            {
+                var primes = _helper.CalcPrimes(from, to);
+
+                invokeOnUiThread(() =>
+                {
+                    foreach (var prime in primes)
+                    {
+                        listBox.Items.Add(prime); 
+                    }
+                });
+            });
+        }
+
+        private void invokeOnUiThread(Action action)
+        {
+            _synchronizationContext.Send(o =>
+            {
+                if (action != null)
+                {
+                    action.Invoke(); 
+                }
+            }, null);
+        }
+
+        private void ValidateNumber(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !number.IsMatch(e.Text);
+        }
+
+        private void textBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
