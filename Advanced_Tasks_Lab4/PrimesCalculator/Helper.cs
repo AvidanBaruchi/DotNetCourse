b19e4906a3790ad35642044e9e9b91abed62472e
Avidan Baruchi <avidanb@code-college.net>
2016-08-13 14:50:50 +0300

    added advanced tasks lab 4

---
+++ Advanced_Tasks_Lab4/PrimesCalculator/Helper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PrimesCalculator
+{
+    class Helper
+    {
+        public async Task<IEnumerable<int>> CalcPrimesAsync(int from, int to, WaitHandle handle)
+        {
+            return await Task.Run(() => CalcPrimesCancelable(from, to, handle));
+        }
+        
+        public IEnumerable<int> CalcPrimes(int from, int to)
+        {
+            return CalcPrimesCancelable(from, to, null);
+        }
+
+        public IEnumerable<int> CalcPrimesCancelable(int from, int to, WaitHandle handle)
+        {
+            List<int> collection = new List<int>();
+
+            if (from < 2)
+            {
+                from = 2;
+            }
+
+            if (from % 2 == 0)
+            {
+                if (from == 2 && to >= from)
+                {
+                    collection.Add(2);
+                }
+
+                from++;
+            }
+
+            for (int currentNumber = from; currentNumber <= to; currentNumber += 2)
+            {
+                if (handle != null 
+                    && handle.WaitOne(0))
+                {
+                    break;
+                }
+
+                if (IsPrime(currentNumber))
+                {
+                    collection.Add(currentNumber);
+                }
+            }
+
+            return collection;
+        }
+
+        private bool IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            if (number == 2)
+            {
+                return true;
+            }
+
+            if (number % 2 == 0)
+            {
+                return false;
+            }
+
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if ((number % i) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Advanced_Tasks_Lab4/PrimesCalculator/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrimesCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Regex number = new Regex("^[0-9]+$");
+        private SynchronizationContext _synchronizationContext = SynchronizationContext.Current;
+        private Helper _helper = new Helper();
+        private CancellationTokenSource _cancellationSource = null;
+        private ObservableCollection<int> _primes = new ObservableCollection<int>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            textBoxFrom.Text = "1";
+            textBoxTo.Text = "5000000";
+            listBox.ItemsSource = _primes;
+        }
+
+        private async void buttonCalc_Click(object sender, RoutedEventArgs e)
+        {
+            buttonCalc.IsEnabled = false;
+            _primes.Clear();
+            await CalculatePrimes();
+            WriteToFile();
+            buttonCalc.IsEnabled = false;
+        }
+
+        private async Task CalculatePrimes()
+        {
+            int from = 0;
+            int to = 0;
+            bool isParsed = int.TryParse(textBoxFrom.Text, out from)
+                && int.TryParse(textBoxTo.Text, out to);
+
+            if (!isParsed)
+            {
+                MessageBox.Show("Please insert a valid numbers!");
+                return;
+            }
+
+            _cancellationSource = new CancellationTokenSource();
+            var token = _cancellationSource.Token;
+
+            var result = await _helper.CalcPrimesAsync(from, to, token.WaitHandle);
+            _primes = new ObservableCollection<int>(result);
+            listBox.ItemsSource = _primes;
+            labelPrimesCount.Content = "Number of Primes: " + _primes.Count;
+        }
+
+        private void WriteToFile()
+        {
+            var fileName = textBoxOutputFile.Text;
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                return;
+            }
+
+            using (var writer = File.AppendText(fileName + ".txt"))
+            {
+                writer.WriteLine();
+            }
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            if (_cancellationSource != null && !_cancellationSource.IsCancellationRequested)
+            {
+                _cancellationSource.Cancel();
+                buttonCalc.IsEnabled = true;
+            }
+        }
+
+        private void InvokeOnUiThread(Action action)
+        {
+            _synchronizationContext.Send(o =>
+            {
+                try
+                {
+                    action?.Invoke();
+                }
+                catch (NotSupportedException e)
+                {
+                    Debug.WriteLine($"Cannot invoke on UI thread! {e.Message}");
+                }
+            }, null);
+        }
+
+        private void ValidateNumber(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !number.IsMatch(e.Text);
+        }
+
+        private void textBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
