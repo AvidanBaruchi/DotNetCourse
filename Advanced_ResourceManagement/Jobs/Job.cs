c45e9b8ad4868ea1bb11224a6dcfadb109bae1ca
Avidan Baruchi <avidanb@code-college.net>
2016-08-14 23:35:27 +0300

    added resources

---
+++ Advanced_ResourceManagement/Jobs/Job.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Jobs
+{
+    static class NativeJob
+    {
+        [DllImport("kernel32")]
+        public static extern IntPtr CreateJobObject(IntPtr sa, string name);
+
+        [DllImport("kernel32", SetLastError = true)]
+        public static extern bool AssignProcessToJobObject(IntPtr hjob, IntPtr hprocess);
+
+        [DllImport("kernel32")]
+        public static extern bool CloseHandle(IntPtr h);
+
+        [DllImport("kernel32")]
+        public static extern bool TerminateJobObject(IntPtr hjob, uint code);
+    }
+
+    public class Job
+    {
+        private IntPtr _hJob;
+        private List<Process> _processes;
+
+        public Job(string name)
+        {
+            _hJob = NativeJob.CreateJobObject(IntPtr.Zero, name);
+
+            if (_hJob == IntPtr.Zero)
+            {
+                throw new InvalidOperationException();
+            }
+
+            AddProcessToJob(_hJob);
+        }
+
+        public Job()
+            : this(null)
+        {
+        }
+
+        protected void AddProcessToJob(IntPtr hProcess)
+        {
+            CheckIfDisposed();
+
+            if (!NativeJob.AssignProcessToJobObject(_hJob, hProcess))
+                throw new InvalidOperationException("Failed to add process to job");
+        }
+
+        private void CheckIfDisposed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddProcessToJob(int pid)
+        {
+            AddProcessToJob(Process.GetProcessById(pid));
+        }
+
+        public void AddProcessToJob(Process proc)
+        {
+            Debug.Assert(proc != null);
+            AddProcessToJob(proc.Handle);
+            _processes.Add(proc);
+        }
+
+        public void Kill()
+        {
+        }
+
+    }
+}
